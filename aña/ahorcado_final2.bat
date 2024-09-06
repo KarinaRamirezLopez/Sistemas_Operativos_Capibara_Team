4161735b2ef11eb9e30aaef3791f98ca8cd752b6
KarinaRamirezLopez <137559120+KarinaRamirezLopez@users.noreply.github.com>
2024-09-06 07:17:11 -0600

    Add files via upload
    aña

---
+++ aña/ahorcado_final2.bat
@@ -0,0 +1,176 @@
+@@echo off
+setlocal enabledelayedexpansion
+Title Capibara Team
+
+:: Cargar archivo de palabras
+set "archivo_palabras=palabras.txt"
+
+:: Verificar si el archivo de palabras existe
+if not exist "%archivo_palabras%" (
+    echo El archivo de palabras "%archivo_palabras%" no existe
+    pause
+    exit /b
+)
+
+:: Elegir una palabra al azar 
+set "num_palabras=0"
+for /f "tokens=*" %%A in ('findstr /n "^" "%archivo_palabras%"') do (
+    set /a "num_palabras+=1"
+    set "palabra_!num_palabras!=%%A"
+)
+
+:: Elegir la palabra
+set /a "palabra_aleatoria=!random! %% num_palabras + 1"
+set "palabra=!palabra_%palabra_aleatoria%!"
+set "strlen=!palabra:~0,1!"
+set "progreso="
+
+:: Inicializar juego
+set "intentos=0"
+set "errores=0"
+set "aciertos=0"
+set "max_aciertos=!strlen!"  :: El max de aciertos es el tamanio de la palabra
+
+:menu
+cls
+color 0B
+echo ===========================================
+echo        Bienvenido al Juego de Ahorcado
+echo       Desarrollado por Team Capibara :]
+echo ===========================================
+echo.
+echo Instrucciones:
+echo Por cada acierto correcto te hacercas a la victoria
+echo Desbloquea la palabra y ganaras
+echo Si juntas 3 errores, pierdes
+echo.
+echo Modo de juego:
+echo 1) Facil
+echo 2) Dificil
+echo 3) Salir
+echo.
+set /p choice="Selecciona el modo de juego (1/2/3): "
+
+if "%choice%"=="1" (
+    color 0A	
+    echo Has seleccionado el modo Facil.
+    set "archivo_preguntas=preguntas_facil.txt"
+    goto start_game
+)
+if "%choice%"=="2" (
+    color 0E
+    echo Has seleccionado el modo Dificil.
+    set "archivo_preguntas=preguntas_dificil.txt"
+    goto start_game
+) 
+if "%choice%"=="3" (
+    echo Adios :)...
+    exit /b
+) else (
+    echo Ingresa una opcion valida
+    pause
+    goto menu
+)
+
+:start_game
+cls
+echo Demos inicio...
+
+:inicio
+cls
+echo ===========================================
+echo          Bienvenido al Juego de Ahorcado
+echo         Desarrollado por Team Capibara :]
+echo ===========================================
+echo.
+echo Palabra: !palabra!
+echo Errores: !errores!
+echo Aciertos: !aciertos! / !max_aciertos!
+echo.
+
+:: Verificar si el archivo de preguntas existe
+if not exist "%archivo_preguntas%" (
+    echo El archivo de preguntas "%archivo_preguntas%" no existe o no se puede acceder
+    pause
+    goto menu
+)
+
+:: Seleccionar un problema al azar
+set "num_preguntas=0"
+for /f "tokens=*" %%A in ('findstr /n "^" "%archivo_preguntas%"') do (
+    set /a "num_preguntas+=1"
+    set "pregunta_!num_preguntas!=%%A"
+)
+
+set /a "pregunta_aleatoria=!random! %% num_preguntas + 1"
+set "pregunta=!pregunta_%pregunta_aleatoria%!"
+set "pregunta_sin_numero=!pregunta:*:=!"
+
+:: Separar la pregunta y las opciones
+for /f "tokens=1,2,3,4,5 delims=|" %%a in ("!pregunta_sin_numero!") do (
+    set "pregunta_texto=%%a"
+    set "opcion1=%%b"
+    set "opcion2=%%c"
+    set "opcion3=%%d"
+    set "respuesta_correcta=%%e"
+)
+
+:: Mostrar la pregunta y opciones
+echo Pregunta: !pregunta_texto!
+echo 1) !opcion1!
+echo 2) !opcion2!
+echo 3) !opcion3!
+set /p "respuesta=Selecciona la respuesta correcta (1/2/3): "
+
+:: Validar que la respuesta 
+if "!respuesta!" neq "1" if "!respuesta!" neq "2" if "!respuesta!" neq "3" (
+    echo Ingresa una opción válida.
+    pause
+    goto inicio
+)
+
+:: Selecion con opcion
+if "!respuesta!"=="1" (
+    set "respuesta_seleccionada=!opcion1!"
+) else if "!respuesta!"=="2" (
+    set "respuesta_seleccionada=!opcion2!"
+) else if "!respuesta!"=="3" (
+    set "respuesta_seleccionada=!opcion3!"
+)
+
+:: Comprobar respuesta con opcion
+set "respuesta_correcta=!respuesta_correcta: =!" :: Elimina espacios en la respuesta c
+set "respuesta_seleccionada=!respuesta_seleccionada: =!" :: Elimina espacios en la respuesta s
+
+:: Comprobacion de la respuesta
+if /i "!respuesta_seleccionada!"=="!respuesta_correcta!" (
+    echo La respuesta es correcta!!
+    set /a "aciertos+=1"
+    pause
+) else (
+    echo La respuesta es incorrecta.
+    set /a "errores+=1"
+    pause
+)
+
+:: Fin del juego si se alcanzan 3 errores
+if %errores% geq 3 (
+    echo Lo siento, has perdido.
+    pause
+    rundll32.exe user32.dll,LockWorkStation
+    start https://i.pinimg.com/1200x/7e/34/04/7e34048a0b17580018a3284e62e3fb13.jpg
+    pause
+    exit /b
+)
+
+:: Verificar ganador
+if %aciertos% geq %max_aciertos% (
+    echo Felicidades, desbloqueaste todos los aciertos permitidos:]
+    pause
+    exit /b
+)
+
+goto inicio
+
+
+
